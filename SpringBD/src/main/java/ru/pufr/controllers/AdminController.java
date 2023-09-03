a71d0b87c79adbb04a9560b183bc782777bb9eba
Serj <mail@pufr.ru>
2023-09-03 17:26:02 +0300

    не работает метод обновления пользователя

--- SpringBD/src/main/java/ru/pufr/controllers/AdminController.java
+++ SpringBD/src/main/java/ru/pufr/controllers/AdminController.java
@@ -5,12 +5,16 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import ru.pufr.models.Status;
 import ru.pufr.models.User;
 import ru.pufr.repo.UserRepository;
 
+import java.util.ArrayList;
+import java.util.Optional;
 import java.util.UUID;
 
 @Controller
@@ -19,11 +23,14 @@ public class AdminController {
     @Autowired
     private UserRepository userRepository;
 
+
     @PreAuthorize("hasAuthority('developers:write')")
     @GetMapping("/enter")
     public String adminHome(Model model) {
-        return "adminkaAdmin";
+        return "admin-page";
     }
+
+
 
     @PreAuthorize("hasAuthority('developers:write')")
     @GetMapping("/users")
@@ -32,6 +39,15 @@ public class AdminController {
         model.addAttribute("users", users);
         return "users-list";
     }
+
+
+    @PreAuthorize("hasAuthority('developers:write')")
+    @GetMapping("/userAdd")
+    public String  userAdd(Model model){
+        return "user-add";
+    }
+
+
 
     @PreAuthorize("hasAuthority('developers:write')")
     @PostMapping("/users/add")
@@ -48,62 +64,101 @@ public class AdminController {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(12);  // для хранения в базе, пароль должен быть закодирован
         String hashedPassword = passwordEncoder.encode(password);
 
-        String str = new String();          // !!!!!! тестовый код !!!! проверка добавления новой колонки в базу
-
-        User user = new User(email, password, firstName, lastName, str, role, status);
-        userRepository.save(user);
-        return "redirect:/users";
-    }
-
-
-    @PreAuthorize("hasAuthority('developers:write')")
-    @GetMapping("/registration")
-    public String getRegistration() {
-
-        return "registration";
-    }
-
-
-    @PreAuthorize("hasAuthority('developers:write')")
-    @PostMapping("/registration")
-    public String registration(@RequestParam String email,
-                               @RequestParam String password,
-                               @RequestParam String firstName,
-                               @RequestParam String lastName,
-                               Model model){
-
-
-        Iterable<User> users = userRepository.findAll();
-
-        for (User user : users) {
-
-            if (user.getEmail().equals(email)) {             // проверим, есть ли в базе такой эмаил.
-                String message = "Пользователь с такой почтой уже есть.";
-
-                model.addAttribute("message", message);
-                return "registration";                  // вернём пользователю ту же страницу, только с сообщением, что такое уже занято
-            }
-        }
-
-
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(12);  // для хранения в базе, пароль должен быть закодирован
-        String hashedPassword = passwordEncoder.encode(password);
-
-        String role = "ADMIN";
-        String status = "ACTIVE";
-
         String uuid = UUID.randomUUID().toString();                     // создаём уникальный номер, для активации
 
         User user = new User(email, hashedPassword, firstName, lastName, uuid, role, status);
         userRepository.save(user);
 
-        // так почему-то не работает
-        //MailController mailController = new MailController();       // вызываем метод отправки писем из контроллера по обслуживанию писем
-        //mailController.sendEmail();
-
-
-        return "redirect:/";                         // при успешной добавке просто переведём на стартовую страницу. !!!!! тестово !!!!!
+        return "redirect:/users";
     }
 
+
+    @PreAuthorize("hasAuthority('developers:write')")
+    @GetMapping("/users/{id}")
+    public String userDetales (@PathVariable(value = "id") long id, Model model) {
+
+        if(!userRepository.existsById(id)){return "redirect:/users";}
+
+        Optional<User> user = userRepository.findById(id);
+        ArrayList<User> res = new ArrayList<>();
+        user.ifPresent(res::add);
+        model.addAttribute("user", res);
+
+        return "user-details";
+    }
+
+
+    @PreAuthorize("hasAuthority('developers:write')")
+    @GetMapping("/user/edit/{id}")
+    public String userUpdate(@PathVariable(value = "id") long id,  Model model) {
+
+        if(!userRepository.existsById(id)){return "redirect:/users";}
+
+        Optional<User> user = userRepository.findById(id);
+        ArrayList<User> res = new ArrayList<>();
+        user.ifPresent(res::add);
+        model.addAttribute("user", res);
+
+        return "user-edit";
+    }
+
+
+    @PreAuthorize("hasAuthority('developers:write')")
+    @PostMapping("/user/edit/{id}")
+    public String userUpdate(@PathVariable(value = "id") long id,
+                              @RequestParam String email,
+                              @RequestParam String password,
+                              @RequestParam String firstName,
+                              @RequestParam String lastName,
+                              @RequestParam String role,
+                              @RequestParam String status,
+                              Model model){
+
+        User user = userRepository.findById(id).<RuntimeException>orElseThrow(() -> {throw new AssertionError();});
+
+        String uuid = UUID.randomUUID().toString();                     // создаём уникальный номер, для активации
+
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setTestCell(uuid);
+        user.setRole(role);
+        user.setStatus(Status.valueOf(status));
+
+        userRepository.save(user);
+
+        return "redirect:/users";
+    }
+
+    @PreAuthorize("hasAuthority('developers:write')")
+    @GetMapping("/user/delete/{id}")
+    public String userDelete(@PathVariable(value = "id") long id, Model model) {
+
+        User user = userRepository.findById(id).<RuntimeException>orElseThrow(() -> {throw new AssertionError();});
+
+        userRepository.delete(user);
+
+        return "redirect:/users";
+    }
+
+
+    @PreAuthorize("hasAuthority('developers:write')")
+    @GetMapping("/user/resetPass/{id}")
+    public String userResetPass(@PathVariable(value = "id") long id,  Model model) {
+
+        if(!userRepository.existsById(id)){return "redirect:/users";}
+
+        User user = userRepository.findById(id).<RuntimeException>orElseThrow(() -> {throw new AssertionError();});
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(12);  // для хранения в базе, пароль должен быть закодирован
+        String hashedPassword = passwordEncoder.encode("password");
+
+        user.setPassword(hashedPassword);
+
+        userRepository.save(user);
+
+        return "redirect:/users";
+    }
 
 }
