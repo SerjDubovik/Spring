1ac1d65e022d991e217012601f1e722291ec9c4b
Serj <mail@pufr.ru>
2023-09-03 13:09:39 +0300

    работа со списком пользователей в локальной бд. рабочий.

--- SpringBD/src/main/java/ru/pufr/controllers/AdminController.java
+++ SpringBD/src/main/java/ru/pufr/controllers/AdminController.java
@@ -11,6 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import ru.pufr.models.User;
 import ru.pufr.repo.UserRepository;
 
+import java.util.UUID;
+
 @Controller
 public class AdminController {
 
@@ -52,5 +54,56 @@ public class AdminController {
         userRepository.save(user);
         return "redirect:/users";
     }
+
+
+    @PreAuthorize("hasAuthority('developers:write')")
+    @GetMapping("/registration")
+    public String getRegistration() {
+
+        return "registration";
+    }
+
+
+    @PreAuthorize("hasAuthority('developers:write')")
+    @PostMapping("/registration")
+    public String registration(@RequestParam String email,
+                               @RequestParam String password,
+                               @RequestParam String firstName,
+                               @RequestParam String lastName,
+                               Model model){
+
+
+        Iterable<User> users = userRepository.findAll();
+
+        for (User user : users) {
+
+            if (user.getEmail().equals(email)) {             // проверим, есть ли в базе такой эмаил.
+                String message = "Пользователь с такой почтой уже есть.";
+
+                model.addAttribute("message", message);
+                return "registration";                  // вернём пользователю ту же страницу, только с сообщением, что такое уже занято
+            }
+        }
+
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(12);  // для хранения в базе, пароль должен быть закодирован
+        String hashedPassword = passwordEncoder.encode(password);
+
+        String role = "ADMIN";
+        String status = "ACTIVE";
+
+        String uuid = UUID.randomUUID().toString();                     // создаём уникальный номер, для активации
+
+        User user = new User(email, hashedPassword, firstName, lastName, uuid, role, status);
+        userRepository.save(user);
+
+        // так почему-то не работает
+        //MailController mailController = new MailController();       // вызываем метод отправки писем из контроллера по обслуживанию писем
+        //mailController.sendEmail();
+
+
+        return "redirect:/";                         // при успешной добавке просто переведём на стартовую страницу. !!!!! тестово !!!!!
+    }
+
 
 }
