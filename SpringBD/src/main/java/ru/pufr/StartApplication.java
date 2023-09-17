207639ec88b59275c26a422bde7cd1c58d1ca796
Serj <mail@pufr.ru>
2023-09-17 15:03:53 +0300

    ещё один способ создать подключение к боту и тоже не работает. не создаёт бин

--- SpringBD/src/main/java/ru/pufr/StartApplication.java
+++ SpringBD/src/main/java/ru/pufr/StartApplication.java
@@ -5,12 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @EnableScheduling
 @SpringBootApplication
 public class StartApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+
+        ApiContextInitializer.init();       // телеграмм бот
+
         SpringApplication.run(StartApplication.class, args);
     }
 
